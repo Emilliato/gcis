e93073bf2e7ed4c7a18cca857933005dab97c05f
Emillio Itai Marambo <emilliomarambo@gmail.com>
2019-08-10 16:52:09 +0200

    Initial commit

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-with open('requirements1.txt', encoding='utf-16') as old, open('requirements.txt', 'w', encoding='utf-8') as new:
+with open('requirements.txt', encoding='utf-16') as old, open('requirements1.txt', 'w', encoding='utf-8') as new:
     new.write(old.read())
